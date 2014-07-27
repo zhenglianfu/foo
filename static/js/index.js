34033fdf9804b9541b5023ae7c78c0d0eaf31a8a
zhenglianfu <15067134439@163.com>
2014-07-27 21:44:52 +0800

    update add event

--- static/js/index.js
+++ static/js/index.js
@@ -2,10 +2,9 @@ simp.ready(function(){
 	simp.require("dom,event", function(data, errors){
 		var dom = data.dom,
 			event = data.event;
-		console.log(dom, event);
 		var container = dom(".container");
 		event.on(container,  "click",  "[data-href]",  function(){
-			
+			window.location.href = this[data-href];
 		});
 	});
 });
