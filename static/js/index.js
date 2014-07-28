bb571fe56ddfdde44909d160d3cf101922d022ae
zhenglianfu <15067134439@163.com>
2014-07-28 09:22:40 +0800

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,5 @@
 simp.ready(function(){
 	simp.require("dom,event", function(data, errors){
-		console.log(data, errors);
 		var dom = data.dom,
 			event = data.event;
 		var container = dom(".container");
