3c2e15ef6c0c2376f7cb05beb53ca7c15b54fe44
zhenglianfu <15067134439@163.com>
2014-07-28 09:19:01 +0800

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,11 @@
 simp.ready(function(){
 	simp.require("dom,event", function(data, errors){
-		console.log(data);
-		console.log(errors);
+		console.log(data, errors);
+		var dom = data.dom,
+			event = data.event;
+		var container = dom(".container");
+		event.on(container,  "click",  "[data-href]",  function(){
+			window.location.href = this[data-href];
+		});
 	});
 });
